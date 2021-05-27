0fe93b9d900b15a78b657ff8a725ea409fbf6fbb
Meg <71366137+meganlynn012@users.noreply.github.com>
2021-05-27 00:10:13 -0600

    week6 display list

--- week6/todo.js
+++ week6/todo.js
@@ -1,5 +1,25 @@
 let todoList = []
 
+function displayList(listText) {
+
+const list = document.querySelector(".listContainer");
+const isChecked = listText.checked ? "done": "";
+const node = document.createElement("li");
+node.setAttribute("class", "todo-item ${isChecked}");
+node.setAttribute("data-key", listText.id);
+
+//adds a checkbox next to each item and shows the item.
+node.innerHTML = `
+    <input id="${listText.id}" type="checkbox"/>
+    <label for="${listText.id}" class="tick js-tick"></label>
+    <span>${listText.item}</span>
+`
+
+//puts the list into the ul element
+list.append(node);
+console.log(todoList);
+}
+
 //transforms the user input into an object and adds it to an array
 function addToArray(item) {
     const listText = {
@@ -7,8 +27,11 @@ function addToArray(item) {
         checked: false,
         id: Date.now()
     }
+    //this syntax adds the user input to the array by pushing to it.
     todoList.push(listText);
-    console.log(todoList);
+
+    //calls the displayList function and sends the array to it.
+    displayList(listText);
 };
 
 //Checks for the user to submit something by pressing enter
