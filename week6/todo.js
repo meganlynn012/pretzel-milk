41133d09c73806587180f539f7a9da8d9251df40
Meg <71366137+meganlynn012@users.noreply.github.com>
2021-05-28 01:44:04 -0600

    todo list with error

--- week6/todo.js
+++ week6/todo.js
@@ -1,55 +1,92 @@
-let todoList = []
+const todoList = [];
 
-function displayList(listText) {
-
-const list = document.querySelector(".listContainer");
-const isChecked = listText.checked ? "done": "";
-const node = document.createElement("li");
-node.setAttribute("class", "todo-item ${isChecked}");
-node.setAttribute("data-key", listText.id);
-
-//adds a checkbox next to each item and shows the item.
-node.innerHTML = `
-    <input id="${listText.id}" type="checkbox"/>
-    <label for="${listText.id}" class="tick js-tick"></label>
-    <span>${listText.item}</span>
-`
-
-//puts the list into the ul element
-list.append(node);
-console.log(todoList);
-}
-
-//transforms the user input into an object and adds it to an array
-function addToArray(item) {
-    const listText = {
-        item,
-        checked: false,
-        id: Date.now()
+class Todo {
+    constructor(item){
+        this.ulElement = item;
     }
-    //this syntax adds the user input to the array by pushing to it.
-    todoList.push(listText);
 
-    //calls the displayList function and sends the array to it.
-    displayList(listText);
-};
+    add() {
+        const input = document.querySelector("#addToList").value;
 
-//Checks for the user to submit something by pressing enter
-const form = document.querySelector(".todo-form");
-form.addEventListener('submit', event => {
-    //this stops the browser from trying to submit the data to a server
-    event.preventDefault();
-    //gets the user input and assigns to usable variables
-    const input = document.querySelector(".todo-input");
-    const item = input.value;
+        //check to make sure the user input something. If not, nothing will happen.
+        if (input !== "") {
+            const todoObject = {
+                id : todoList.length,
+                todoText : input,
+                isDone : false,
+            }
 
-    //makes sure user entered something, and passes it to the array function
-if (item !== "") {
-    addToArray(item);
+            //push will add an item to the bottom of the array
+            //unshift will add it to the top
+            todoList.unshift(todoObject);
+            this.display();
+            //resets the text
+            document.querySelector("#addToList").value = "";
+        }
 
-    //clears the text box for the user and focuses the element. These are cool.
-    input.value = "";
-    input.focus();
+    }
+
+    check(index) {
+        //finds the matching index number of the array when the list item is clicked.
+        const selectedIndex = todoList.findIndex((item)=> item.id == index);
+        todoList[selectedIndex].isDone == false ? todoList[selectedIndex].isDone = true
+        : todoList[selectedIndex].isDone = false;
+        this.display();
+    }
+
+    delete(index) {
+        const selectedDelIndex = todoList.findIndex((item)=> item.id == index);
+        todoList.splice(selectedDelIndex,1);
+        this.display();
+
+    }
+
+    display() {
+        //clears the content of the array so it doesn't display the entire array each time.
+        this.ulElement.innerHTML = "";
+
+        todoList.forEach((objectItem) => {
+            const li = document.createElement("li");
+            //must be an i since the icon code is <i class="fas fa-eraser"></i>
+            const delBtn = document.createElement("i");
+            //will this work as text content?
+            li.innerText = objectItem.todoText;
+            li.setAttribute("dataId", objectItem.id);
+
+            delBtn.setAttribute("dataId", objectItem.id);
+            delBtn.classList.add("fas", "fa-eraser");
+
+            //append the delete button to the list item
+            li.appendChild(delBtn);
+
+            //delete the list item when the icon is clicked.
+            delBtn.addEventListener("click", function(e) {
+                const deleteId = e.target.getAttribute("dataId");
+                mytodoList.delete(deleteId);
+            })
+
+            li.addEventListener("click", function(e) {
+                const selectedId = e.target.getAttribute("dataId");
+                mytodoList.check(selectedId);
+            })
+
+            //add the checked class to an item if the object property 'isDone' is true
+            if (objectItem.isDone) {
+                li.classList.add("checked");
+            }
+
+            //appends the list item to the ul list.
+            this.ulElement.appendChild(li);
+        })
+    }
 }
-console.log(item);
+
+//assigns the ul element to listSection
+const listSection = document.querySelector("#listContainer");
+
+//assigns the entire class Todo to a variable and passes the ul element to it as a parameter
+mytodoList = new Todo(listSection);
+
+document.querySelector(".addBtn").addEventListener("click", function() {
+    mytodoList.add()
 });
