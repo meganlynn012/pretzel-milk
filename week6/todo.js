dbceb7f5cb68291909a22cf682f377bade1fe0b4
Meg <71366137+meganlynn012@users.noreply.github.com>
2021-06-03 17:36:17 -0600

    todo app done

--- week6/todo.js
+++ week6/todo.js
@@ -1,123 +1,120 @@
-todoList = {
-    showDone: false,
-    todoList: [],
-}
+let todoList = []
 
-const todos = todoList.todoList;
-
-class Todo {
-    constructor(item) {
-        this.ulElement = item;
-    }
-
-    add() {
-        const input = document.querySelector("#addToList").value;
-
-        //check to make sure the user input something. If not, nothing will happen.
-        if (input !== "") {
-            const todoObject = {
-                id: todos.length,
-                todoText: input,
-                isDone: false,
-            }
-
-            //push will add an item to the bottom of the array
-            //unshift will add it to the top
-            todos.push(todoObject);
-            localStorage.setItem("listContainer", JSON.stringify(todoObject));
-            this.display();
-            //resets the text
-            document.querySelector("#addToList").value = "";
-        }
-
-    }
-
-    check(index) {
-        //finds the matching index number of the array when the list item is clicked.
-        const selectedIndex = todos.findIndex((item) => item.id == index);
-        todos[selectedIndex].isDone == false ? todos[selectedIndex].isDone = true :
-            todos[selectedIndex].isDone = false;
-        this.display();
-    }
-
-    delete(index) {
-        const selectedDelIndex = todos.findIndex((item) => item.id == index);
-        todos.splice(selectedDelIndex, 1);
-        this.display();
-
-    }
-
-    filter() {
-        todoList.showDone == false ? todoList.showDone = true : todoList.showDone = false;
-        //console.log(todoList.showDone);
-        if (todoList.showDone == false) {
-            let doneList = todos.filter(done => done.isDone == true);
-            console.log(doneList);
-        }
-    };
-
-    display() {
-        //clears the content of the array so it doesn't display the entire array each time.
-        this.ulElement.innerHTML = "";
-
-        todos.forEach((objectItem) => {
-            const li = document.createElement("li");
-
-            const delBtn = document.createElement("span");
-
-            li.innerText = objectItem.todoText;
-            li.setAttribute("dataId", objectItem.id);
-
-            delBtn.setAttribute("dataId", objectItem.id);
-            delBtn.classList.add("x");
-
-            //append the delete button to the list item
-            li.appendChild(delBtn);
-
-            //delete the list item when the icon is clicked.
-            delBtn.addEventListener("click", function (e) {
-                const deleteId = e.target.getAttribute("dataId");
-                mytodoList.delete(deleteId);
-            })
-
-            li.addEventListener("click", function (e) {
-                const selectedId = e.target.getAttribute("dataId");
-                mytodoList.check(selectedId);
-            })
-
-            //add the checked class to an item if the object property 'isDone' is true
-            if (objectItem.isDone) {
-                li.classList.add("checked");
-            }
-
-
-            //appends the list item to the ul list.
-            this.ulElement.appendChild(li);
-
-            document.querySelector("#completeBtn").addEventListener("click", function (e) {
-                mytodoList.filter();
-
-            });
-        })
-    }
-}
-
-//assigns the ul element to listSection
-const listSection = document.querySelector("#listContainer");
-
-//assigns the entire class Todo to a variable and passes the ul element to it as a parameter
-mytodoList = new Todo(listSection);
-
-document.querySelector(".addBtn").addEventListener("click", function () {
-    mytodoList.add()
-
-});
-
-//this will allow the user to add items by pressing enter
-document.querySelector(".inputContainer").addEventListener("submit", event => {
-    //stops the browser from trying to submit the form to a server
+//calls the add function when user hits enter.
+document.querySelector(".inputContainer").addEventListener("submit", (event) => {
+    //stops the browser from submitting the form to a server
     event.preventDefault();
-    mytodoList.add()
+    add();
+})
+//calls the add function when user clicks/touches the plus button.
+document.getElementById("addButton").addEventListener("click", (event) => {
+    event.preventDefault();
+    add();
+})
+
+function add() {
+    const input = document.getElementById("addToList").value;
+    //console.log(input);
+
+    if (input !== "") {
+        const todoObject = {
+            id: Date.now(),
+            text: input,
+            isDone: false,
+        }
+        //adds the user input into the array todoList.
+        todoList.push(todoObject);
+        //console.log(todoList);
+        storage(todoList);
+        //resets the input line so the user doesn't have to delete the last input
+        document.getElementById("addToList").value = "";
+    }
+};
+
+function storage(todoList){
+        //stores inputs in localStorage
+        localStorage.setItem("todoList", JSON.stringify(todoList));
+        display(todoList);
+}
+function getStorage(){
+        const reference = localStorage.getItem("todoList");
+        if (reference) {
+        todoList = JSON.parse(reference);
+
+        //calls the display function to display the array in localStorage
+        display(todoList);
+        }
+};    
+
+function display(todoList) {
+    //clears everything in the ul so it doesn't repeat the entire array each time.
+    document.getElementById("listContainer").innerHTML = "";
+
+    todoList.forEach((item) => {
+        const checked = item.isDone ? 'checked': null;
+
+        const li = document.createElement("li");
+        li.setAttribute("data-key", item.id);
+        if(item.isDone === true) {
+            li.classList.add('checked');
+        }
+
+        li.innerHTML = '<span class="checkbox"></span>' + '<p class="list-item">' + item.text + '</p>' +
+        '<span id="x" class="delete"></span>';
+
+        //display the list by appending the li to the ul
+        document.getElementById("listContainer").appendChild(li);
+    });
+}
+
+getStorage();
+
+document.querySelector("#listContainer").addEventListener("click", (event) => {
+    if (event.target.classList.contains('checkbox')) {
+        let selectedId = event.target.parentElement.getAttribute('data-key');
+        //console.log(selectedId);
+        check(selectedId);
+    }
+
+    if (event.target.classList.contains('delete')) {
+        let deleteId = event.target.parentElement.getAttribute('data-key');
+        //console.log(deleteId);
+        remove(deleteId);
+    }
+})
+
+function check(id) {
+    todoList.forEach(item => {
+        if (item.id == id) {
+            item.isDone = !item.isDone;
+            //console.log(todoList);
+        }
+    })
+    storage(todoList);
+};
+
+function remove(id) {
+    const index = todoList.findIndex(item => item.id === Number(id));
+    //console.log(index);
+    const todo = {
+        deleted: true,
+        ...todoList[index]
+    };
+    todoList = todoList.filter( item => item.id !== Number(id));
+    //console.log(todoList);
+    storage(todoList);
+}
+
+function complete() {
+    let todoDone = todoList.filter(item => item.isDone);
+    //console.log(todoDone);
+    display(todoDone);
+}
+
+function inProgress() {
+    let todoStill = todoList.filter(item => !item.isDone);
+    //console.log(todoStill);
+    display(todoStill);
+}
 
 
-});
