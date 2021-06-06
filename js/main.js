d494785460bcff072c9666bbccfcfb6a3eb514d2
Meg <71366137+meganlynn012@users.noreply.github.com>
2021-06-05 23:43:40 -0600

    week7 link added to index

--- js/main.js
+++ js/main.js
@@ -29,7 +29,8 @@ const links = [
       '<a href="week3/week3.html">Week 3</a>',
       '<a href="week4/week4.html">Week 4</a>',
       '<a href="week5/week5.html">Week 5</a>',
-      '<a href="week6/week6.html">Week 6</a>'
+      '<a href="week6/week6.html">Week 6</a>',
+      '<a href="week7/week7.html">Week 7</a>'
     
   ];
 
