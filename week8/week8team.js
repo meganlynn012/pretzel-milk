b7c88036818abba04b087dce8bd51fb4eab443e1
Meg <71366137+meganlynn012@users.noreply.github.com>
2021-06-12 01:30:00 -0600

    styling added

--- week8/week8team.js
+++ week8/week8team.js
@@ -1,31 +1,38 @@
-let nexturl = `https://swapi.dev/api/people`;
-let previousurl = null;
+let nextURL = "https://swapi.dev/api/people";
+let previousURL = null;
 
-function fetchPeople(next=true) {
-    let url="";
-    if (next) {
-        url = nexturl;
+// Fetch a list of people from the Star Wars API.
+// The "next" parameter tells the function whether to fetch the next list of 10 people,
+// or the previous list of 10 people.
+function fetchPeople(next=true)
+{
+    let url = "";
+    if (next)
+    {
+        url = nextURL;
     }
-    else {
-        url = previousurl
+    else
+    {
+        url = previousURL;
     }
-    fetch(url)
-    .then(response => response.json())
-    .then(json => {
-        previousurl = json["previous"];
-        nexturl = json["next"];
-
-        document.getElementById("next").disabled = nexturl ? false: true;
-        document.getElementById("previous").disabled = previousurl ? false : true;
+    fetch(url).then(response => response.json()).then(json => {
+       previousURL = json["previous"];
+        nextURL = json["next"];
+        // Disable "next" & "previous" buttons if there is no use for them.
+        document.getElementById("next").disabled = nextURL ? false : true;
+        document.getElementById("previous").disabled = previousURL ? false : true;
         return displayPeople(json["results"]);
-    })
-    }
-    function displayPeople(peopleList) {
-        document.getElementById("descriptionDiv").innerHTML = "";
-        for (const i in peopleList) {
+    });
+}
+// Displays the list of people fetched by fetchPeople.
+function displayPeople(peopleList)
+{
+    document.getElementById("descriptionDiv").innerHTML = "";
+    for (const i in peopleList)
+    {
         const person = peopleList[i];
         const personDisplayElement = document.createElement("p");
-        personDisplayElement.innerHTML = `${person["name"]}`
+        personDisplayElement.innerHTML = person["name"];
         document.getElementById("descriptionDiv").appendChild(personDisplayElement);
     }
-};
+}
